e657bb3b0feea7aa6e801ee8b1cb3908bc092485
unknown <s.jumadil.akbar@gmail.com>
2023-02-17 15:50:43 +0700

    17 Feb 2023

--- start/hook/index.ts
+++ start/hook/index.ts
@@ -26,3 +26,29 @@ View.global('tanggal_laporan', function(tanggal : Date){
     const year = tanggal.getFullYear()
     return `${date}/${month}/${year}`
 })
+
+View.global('tandaPemisahTitik', function(b){
+    if (b == null) {
+        return 0
+    }
+    var _minus = false;
+    if (b<0) _minus = true;
+    b = b.toString();
+    b=b.replace(".","");
+    b=b.replace("-","");
+    let c = "";
+    let panjang = b.length;
+    let j = 0;
+    for (let i = panjang; i > 0; i--){
+        j = j + 1;
+        if (((j % 3) == 1) && (j != 1)){
+            c = b.substr(i-1,1) + "." + c;
+        } else {
+            c = b.substr(i-1,1) + c;
+        }
+    }
+    if (_minus) c = "-" + c ;
+    return c;
+})
+
+
